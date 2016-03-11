662c5d6ccf4950b64172b0c654e1b869793b16c1
shanelau <shanelau1021@gmail.com>
2016-03-11 20:30:35 +0800

    add cron parse support

--- api/CronServer.js
+++ api/CronServer.js
@@ -75,6 +75,21 @@ client2.on("pmessage", function (pattern, channel, message) {
 });
 
 /**
+ * 当前的任务还未结束 获取下一次定时任务的时间
+ * @param interval interval 对象
+ * @param next 当前的时间
+ */
+function getNextDate(interval, next){
+  var date = new Date(next.toString());
+  var timeOffset = moment(date).unix() - moment().unix();
+  if (timeOffset < 1) {
+    next = interval.next();
+    return getNextDate(interval, next);
+  }
+  return timeOffset;
+}
+
+/**
  * 检查定时任务的时间是否过期
  * @param rule  *    *    *    *    *    *
  * @return string | boolean 过期返回 false, 否则返回过期的毫秒数
@@ -82,11 +97,11 @@ client2.on("pmessage", function (pattern, channel, message) {
 function checkRule(rule){
   var interval = parser.parseExpression(rule);
   var next = interval.next();
+  console.log(new Date().getTime(),new Date(next.getTime()).getTime());
   if (next.done) {
     return false;
   }else {
-    var date = new Date(next.toString());
-    return moment(date).unix() - moment().unix();
+    return getNextDate(interval, next);
   }
 }
 
@@ -97,7 +112,8 @@ function addTask(message) {
     obj  = JSON.parse(message);
     console.log('received ' + obj.app + '\'s task ' + obj.method);
     var ttl = checkRule(obj.rule);
-    if (ttl) {
+    console.log('next: ',ttl);
+    if (ttl && ttl > 0) {
       console.log('task will be run after %s second', ttl);
       client3.set(getAppSystemKey(obj.app), obj.app); // 注册 app, 设置任务的 app 到 app 集合,用来做分析处理
       client3.setex(TASK_PREFIX + obj.app + ':' + message, ttl, ''); // 设置过期时间
--- test/other.js
+++ test/other.js
@@ -3,16 +3,17 @@
  */
 var parser = require('cron-parser');
 var moment = require('moment');
-try {
-  var interval = parser.parseExpression('*/2 * * * *');
 
-  console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
-  console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:44:00 GMT+0200 (EET)
-  console.log('Date: ', interval.next().toUTC().toString()); // Sat Dec 28 2012 22:46:00 GMT+0200 (EET)
-} catch (err) {
-  console.log('Error: ' + err.message);
+function parse(){
+  try {
+    var interval = parser.parseExpression('59 */1 * * * *');
+
+    console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+    //console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:44:00 GMT+0200 (EET)
+  } catch (err) {
+    console.log('Error: ' + err.message);
+  }
+
+  var obj = moment().add(11, 'second').unix();
+  console.log(obj - moment().unix());
 }
-
-var obj = moment().add(11, 'second').unix();
-console.log(obj);
-console.log(obj - moment().unix());
